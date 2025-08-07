8707a788f670084f2aeeb92bccd778a6132213e6
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-07 13:48:22 -0500

    Init project

---
+++ src/test/java/com/lian/marketing/paymentmicroservice/PaymentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lian.marketing.paymentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
