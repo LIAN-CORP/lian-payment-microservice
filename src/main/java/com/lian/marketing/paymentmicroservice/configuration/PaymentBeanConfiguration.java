9a6cec717301b8b1ec18af98ff3f8ff65c043848
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-09 08:20:58 -0500

    feat(configuration): Add PaymentBeanConfiguration to wire domain and infrastructure layers

---
+++ src/main/java/com/lian/marketing/paymentmicroservice/configuration/PaymentBeanConfiguration.java
@@ -0,0 +1,30 @@
+package com.lian.marketing.paymentmicroservice.configuration;
+
+import com.lian.marketing.paymentmicroservice.domain.api.IPaymentServicePort;
+import com.lian.marketing.paymentmicroservice.domain.api.usecase.PaymentUseCase;
+import com.lian.marketing.paymentmicroservice.domain.spi.IPaymentPersistencePort;
+import com.lian.marketing.paymentmicroservice.infrastructure.driven.jpa.postgres.adapter.PaymentAdapter;
+import com.lian.marketing.paymentmicroservice.infrastructure.driven.jpa.postgres.mapper.IPaymentEntityMapper;
+import com.lian.marketing.paymentmicroservice.infrastructure.driven.jpa.postgres.repository.IPaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class PaymentBeanConfiguration {
+
+    private final IPaymentRepository paymentRepository;
+    private final IPaymentEntityMapper paymentEntityMapper;
+
+    @Bean
+    public IPaymentPersistencePort paymentPersistencePort() {
+        return new PaymentAdapter(paymentEntityMapper, paymentRepository);
+    }
+
+    @Bean
+    public IPaymentServicePort paymentServicePort() {
+        return new PaymentUseCase(paymentPersistencePort());
+    }
+
+}
