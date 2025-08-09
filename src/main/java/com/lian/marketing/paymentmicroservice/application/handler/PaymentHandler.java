50c67cb0f5d49dc484f198224d9fcb61f1b18db6
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-09 08:21:23 -0500

    refactor(application): Remove unused PaymentUseCase import in PaymentHandler

--- src/main/java/com/lian/marketing/paymentmicroservice/application/handler/PaymentHandler.java
+++ src/main/java/com/lian/marketing/paymentmicroservice/application/handler/PaymentHandler.java
@@ -3,7 +3,6 @@ package com.lian.marketing.paymentmicroservice.application.handler;
 import com.lian.marketing.paymentmicroservice.application.dto.request.CreatePaymentRequest;
 import com.lian.marketing.paymentmicroservice.application.mapper.IPaymentMapper;
 import com.lian.marketing.paymentmicroservice.domain.api.IPaymentServicePort;
-import com.lian.marketing.paymentmicroservice.domain.api.usecase.PaymentUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
