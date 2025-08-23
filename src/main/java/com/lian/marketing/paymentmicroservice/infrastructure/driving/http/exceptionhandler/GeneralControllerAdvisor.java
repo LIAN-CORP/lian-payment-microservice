29cac1c4025937ed9a05e5c315e996921ee8aff5
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-23 17:09:07 -0500

    feat(infrastructure): handle MethodArgumentNotValidException in GeneralControllerAdvisor

--- src/main/java/com/lian/marketing/paymentmicroservice/infrastructure/driving/http/exceptionhandler/GeneralControllerAdvisor.java
+++ src/main/java/com/lian/marketing/paymentmicroservice/infrastructure/driving/http/exceptionhandler/GeneralControllerAdvisor.java
@@ -14,7 +14,7 @@ public class GeneralControllerAdvisor {
                 .body(new ExceptionResponse(
                         HttpStatus.BAD_REQUEST.toString(),
                         HttpStatus.BAD_REQUEST.value(),
-                        exception.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                        exception.getBindingResult().getAllErrors().getFirst().getDefaultMessage(),
                         java.time.LocalDateTime.now(),
                         "METHOD_ARGUMENT_NOT_VALID_EXCEPTION"
                 ));
