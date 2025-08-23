1687bb89472947ac37d471243edf610a44d6985a
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-23 17:08:54 -0500

    feat(infrastructure): handle MethodArgumentNotValidException in GeneralControllerAdvisor

--- src/main/java/com/lian/marketing/paymentmicroservice/infrastructure/driving/http/exceptionhandler/GeneralControllerAdvisor.java
+++ src/main/java/com/lian/marketing/paymentmicroservice/infrastructure/driving/http/exceptionhandler/GeneralControllerAdvisor.java
@@ -1,8 +1,22 @@
 package com.lian.marketing.paymentmicroservice.infrastructure.driving.http.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GeneralControllerAdvisor {
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(
+                        HttpStatus.BAD_REQUEST.toString(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        exception.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                        java.time.LocalDateTime.now(),
+                        "METHOD_ARGUMENT_NOT_VALID_EXCEPTION"
+                ));
+    }
 }
