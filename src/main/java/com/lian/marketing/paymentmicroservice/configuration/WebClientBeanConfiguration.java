5a312dd08d9636f636af93dccff02c72f35d5a64
3lPr1mo <erickenriquechaparro@gmail.com>
2026-01-25 13:20:28 -0500

    feat(config): externalize transaction API URL in configuration
    - Added `url.transaction.api` property in `application.properties` for external configuration.
    - Updated `WebClientBeanConfiguration` to use the new property for `baseUrl`.

--- src/main/java/com/lian/marketing/paymentmicroservice/configuration/WebClientBeanConfiguration.java
+++ src/main/java/com/lian/marketing/paymentmicroservice/configuration/WebClientBeanConfiguration.java
@@ -1,6 +1,7 @@
 package com.lian.marketing.paymentmicroservice.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,10 +10,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 public class WebClientBeanConfiguration {
 
+    @Value("${url.transaction.api}")
+    private String transactionApi;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8082")
+                .baseUrl(transactionApi)
                 .build();
     }
 
